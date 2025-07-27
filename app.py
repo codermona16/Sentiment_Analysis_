b0060b4900935b2ddfb9584e96592164ea8c97b9
Monalisa Dutta <144586268+codermona16@users.noreply.github.com>
2025-07-28 02:04:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,34 +1,40 @@
 import streamlit as st
 import pickle
 import gdown
+import os
 
 
-file_id = "1ssbYGj7afMLptc9PKGNKCSnmY7JZXKPn"
-url = f"https://drive.google.com/uc?export=download&id={file_id}"
-output = "best_model.pkl"
+MODEL_FILE_ID = "1ssbYGj7afMLptc9PKGNKCSnmY7JZXKPn"
+MODEL_FILE = "best_model.pkl"
 
-
-gdown.download(url, output, quiet=False)
-
+def download_if_not_exists(file_id, output):
+    if not os.path.exists(output):
+        url = f"https://drive.google.com/uc?export=download&id={file_id}"
+        gdown.download(url, output, quiet=False)
 
 def load_pickle(filepath):
     with open(filepath, "rb") as file:
         return pickle.load(file)
 
-model = load_pickle(output)
-vectorizer = load_pickle("C:/Users/ASUS/OneDrive/Desktop/Sentiment_Analysis_/tfidf_vectorizer.pkl")
-le = load_pickle("C:/Users/ASUS/OneDrive/Desktop/Sentiment_Analysis_/label_encoder.pkl")
+download_if_not_exists(MODEL_FILE_ID, MODEL_FILE)
+
+
+model = load_pickle(MODEL_FILE)
+
+vectorizer = load_pickle("tfidf_vectorizer.pkl")
+le = load_pickle("label_encoder.pkl")
 
 
 
-# ✅ Function to predict sentiment
+
+
 def predict_sentiment(text):
     text_vect = vectorizer.transform([text])  # Transform user input
     pred_encoded = model.predict(text_vect)   # Predict encoded label
     pred_label = le.inverse_transform(pred_encoded)  # Convert back to text label
     return pred_label[0]
 
-# ✅ Streamlit UI
+
 st.title("📊 Sentiment Analysis App")
 st.write("Enter a review or any text below to analyze its sentiment.")
 
